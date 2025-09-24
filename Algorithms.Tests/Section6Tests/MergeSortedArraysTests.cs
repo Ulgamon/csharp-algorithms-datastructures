cd54c1a61bf60072aef2e5d5012d5a9f734024d8
Ulgamon <ilicjustin@gmail.com>
2025-09-24 20:06:15 +0200

    Merge sorted arrays.

---
+++ Algorithms.Tests/Section6Tests/MergeSortedArraysTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Section6;
+
+namespace Algorithms.Tests.Section6Tests
+{
+    public class MergeSortedArraysTests
+    {
+        [Fact]
+        public void TestMergeSortedArrays()
+        {
+            int[] arr1 = [0, 3, 4, 31];
+            int[] arr2 = [4, 6, 30];
+
+            int[] correctResult = [0, 3, 4, 4, 6, 30, 31];
+
+            int[] result = MergeSortedArrays.MergeSortedArraysMethod(arr1, arr2);
+
+            Assert.Equal(correctResult, result);
+
+        }
+
+        [Fact]
+        public void TestMergeSortedArraysOneElement()
+        {
+            int[] arr1 = [0];
+            int[] arr2 = [4];
+
+            int[] correctResult = [0, 4];
+
+            int[] result = MergeSortedArrays.MergeSortedArraysMethod(arr1, arr2);
+
+            Assert.Equal(correctResult, result);
+
+        }
+
+        [Fact]
+        public void TestMergeSortedArraysEmptyArrays()
+        {
+            int[] arr1 = [];
+            int[] arr2 = [];
+
+            int[] correctResult = [];
+
+            int[] result = MergeSortedArrays.MergeSortedArraysMethod(arr1, arr2);
+
+            Assert.Equal(correctResult, result);
+
+        }
+
+    }
+}
---
+++ Algorithms/Section6/MergeSortedArrays.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Section6
+{
+    public class MergeSortedArrays
+    {
+        //int[] arr1 = [0, 3, 4, 31];
+        //int[] arr2 = [4, 6, 30];
+        // Whiile loop will exit on the first index that goes out of bounds
+        // so what that means is that one array is finished and other is not
+        // so I need to loop through that second array and append remaining elements.
+
+        // Time complexity is O(a + b)
+
+        public static int[] MergeSortedArraysMethod(int[] arr1, int[] arr2)
+        {
+            int[] mergedArray = new int[arr1.Length + arr2.Length];
+
+            int i = 0;
+            int j = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] < arr2[j])
+                {
+                    mergedArray[i + j] = arr1[i];
+                    i++;
+                }
+                else
+                {
+                    mergedArray[i + j] = arr2[j];
+                    j++;
+                }
+            }
+            if (i < arr1.Length)
+            {
+                for (; i < arr1.Length; i++)
+                {
+                    mergedArray[i + j] = arr1[i];
+                }
+            }
+            else
+            {
+                for (; j < arr2.Length; j++)
+                {
+                    mergedArray[i + j] = arr2[j];
+                }
+            }
+            return mergedArray;
+        }
+    }
+}
