afe1ad2815251279360ecf841070a7e9a39d8de2
Ulgamon <ilicjustin@gmail.com>
2025-11-14 18:46:54 +0100

    Append tested and written.

---
+++ Algorithms.Tests/Section8Tests/DoublyLinkedListTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Section8;
+
+namespace Algorithms.Tests.Section8Tests
+{
+    public class DoublyLinkedListTests
+    {
+        [Fact]
+        public void TestLinkedLists1()
+        {
+            // Creating array of links for Linked List constructor
+            DoublyLink<int>[] test = [new DoublyLink<int>(1), new DoublyLink<int>(2), new DoublyLink<int>(3), new DoublyLink<int>(4), new DoublyLink<int>(0), new DoublyLink<int>(-1), new DoublyLink<int>(80)];
+
+            DoublyLinkedList<int> ints = new DoublyLinkedList<int>(test);
+
+            // Creating a test list
+            List<int> list = new List<int> { 1, 2, 3, 4, 0, -1, 80 };
+
+            Assert.Equal(ints.ReturnList(), list);
+        }
+
+        [Fact]
+        public void TestLinkedListsAppend()
+        {
+            // Creating array of links for Linked List constructor
+            DoublyLink<int>[] test = [new DoublyLink<int>(1), new DoublyLink<int>(2), new DoublyLink<int>(3), new DoublyLink<int>(4), new DoublyLink<int>(0), new DoublyLink<int>(-1), new DoublyLink<int>(80)];
+
+            DoublyLinkedList<int> ints = new DoublyLinkedList<int>(test);
+
+            // Creating a test list
+            List<int> list = new List<int> { 1, 2, 3, 4, 0, -1, 80, 69 };
+
+            ints.Append(new DoublyLink<int>(69));
+
+            Assert.Equal(ints.ReturnList(), list);
+        }
+    }
+}
---
+++ Algorithms/Section8/DoublyLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Section8
+{
+    public class DoublyLinkedList<T>
+    {
+        DoublyLink<T>? Head { get; set; }
+        DoublyLink<T>? Tail { get; set; }
+        int Length { get; set; }
+
+        public DoublyLinkedList(params DoublyLink<T>[] values)
+        {
+            if (values == null || values.Length == 0)
+            {
+                Head = null;
+                Tail = null;
+                Length = 0;
+            }
+            else
+            {
+                Head = values[0];
+                Tail = values[values.Length - 1];
+                Length = values.Length;
+                for (int i = 0; i < values.Length - 1; i++)
+                {
+                    values[i].Next = values[i + 1];
+                }
+                for (int j = values.Length - 1; j > 0; j--)
+                {
+                    values[j].Previous = values[j - 1];
+                }
+            }
+        }
+        public List<T> ReturnList()
+        {
+            List<T> list = new List<T>();
+            if (Length > 0)
+            {
+                DoublyLink<T> link = Head;
+                while (link != null)
+                {
+                    list.Add(link.Value);
+                    link = link.Next;
+                }
+            }
+            return list;
+        }
+
+        public DoublyLink<T>? Append(DoublyLink<T> link)
+        {
+            if (Length == 0)
+            {
+                Head = link;
+                Tail = link;
+                Length++;
+                return link;
+            }
+            link.Previous = Tail;
+            Tail.Next = link;
+            Tail = link;
+            return link;
+        }
+    }
+
+    public class DoublyLink<T>
+    {
+        public DoublyLink<T>? Next { get; set; } = default;
+        public DoublyLink<T>? Previous { get; set; } = default;
+        public T Value { get; set; }
+
+        public DoublyLink(T value, DoublyLink<T>? next = default, DoublyLink<T>? previous = default)
+        {
+            Value = value;
+            Next = next;
+            Previous = previous;
+        }
+    }
+}
