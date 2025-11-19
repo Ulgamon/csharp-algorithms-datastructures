7499c68cd02c834e3fa66dd18c79e808f5f2d13a
Ulgamon <ilicjustin@gmail.com>
2025-11-19 11:18:24 +0100

    Stack constructor  and push tested and working.

---
+++ Algorithms.Tests/Section9Tests/StackImplementationTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Section8;
+using Algorithms.Section9;
+
+namespace Algorithms.Tests.Section9Tests
+{
+    public class StackImplementationTests
+    {
+        [Fact]
+        public void TestStack()
+        {
+            // Creating array of links for Linked List constructor
+            StackNode<int>[] test = [new StackNode<int>(1), new StackNode<int>(2), new StackNode<int>(3), new StackNode<int>(4), new StackNode<int>(0), new StackNode<int>(-1), new StackNode<int>(80)];
+
+            StackImplementation<int> ints = new StackImplementation<int>();
+
+            ints.Push(new StackNode<int>(80));
+            ints.Push(new StackNode<int>(-1));
+            ints.Push(new StackNode<int>(0));
+            ints.Push(new StackNode<int>(4));
+            ints.Push(new StackNode<int>(3));
+            ints.Push(new StackNode<int>(2));
+            ints.Push(new StackNode<int>(1));
+
+            // Creating a test list
+            List<int> list = new List<int> { 1, 2, 3, 4, 0, -1, 80 };
+
+            Assert.Equal(ints.ReturnList(), list);
+        }
+    }
+}
---
+++ Algorithms/Section9/StackImplementation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Section9
+{
+    public class StackImplementation<T>
+    {
+        StackNode<T>? Top { get; set; }
+        StackNode<T>? Bottom { get; set; }
+        int Length { get; set; }
+
+        public StackImplementation()
+        {
+            Top = null;
+            Bottom = null;
+            Length = 0;
+        }
+
+        //public StackNode<T>? Peek(int index)
+        //{
+
+        //}
+
+        public StackNode<T>? Push(StackNode<T> value)
+        {
+            if (Length == 0)
+            {
+                Top = value;
+                Bottom = value;
+                Length++;
+                return value;
+            }
+            value.Next = Top;
+            Top = value;
+            return value;
+        }
+
+        //public StackNode<T>? Pop()
+        //{
+        //    if (Length == 1 && Top != null)
+        //    {
+        //        StackNode<T> res = Top;
+        //        Top = null;
+        //        Bottom = null;
+        //        Length = 0;
+        //        return res;
+        //    }
+        //    if (Length == 0 || Top == null) return null;
+
+        //    StackNode<T> result = Top;
+        //    Top = Top.Next;
+        //    return result;
+
+        //}
+
+        public List<T> ReturnList()
+        {
+            List<T> result = new List<T>();
+            if (Top == null || Length == 0) return result;
+            StackNode<T>? iter = Top;
+            while(iter != null)
+            {
+                result.Add(iter.Value);
+                iter = iter.Next;
+            }
+            return result;
+        }
+    }
+
+    public class StackNode<T>
+    {
+        public T Value { get; set; }
+        public StackNode<T>? Next { get; set; }
+
+        public StackNode(T value, StackNode<T>? next = null)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}
