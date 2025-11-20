73fff711c9576615f363c4509ad737ee12b45575
Ulgamon <ilicjustin@gmail.com>
2025-11-20 08:50:13 +0100

    Stack with dynamic array implemented and tested.

---
+++ Algorithms.Tests/Section9Tests/StackArrayTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Section9;
+
+namespace Algorithms.Tests.Section9Tests
+{
+    public class StackArrayTests
+    {
+        [Fact]
+        public void TestStack()
+        {
+            StackArray<int> ints = new StackArray<int>();
+
+            ints.Push(1);
+            ints.Push(2);
+            ints.Push(3);
+            ints.Push(4);
+            ints.Push(0);
+            ints.Push(-1);
+            ints.Push(80);
+
+            // Creating a test list
+            List<int> list = new List<int> { 1, 2, 3, 4, 0, -1, 80 };
+
+            Assert.Equal(ints.StackValues, list);
+        }
+
+        [Fact]
+        public void TestStackPop()
+        {
+            StackArray<int> ints = new StackArray<int>();
+
+            ints.Push(1);
+            ints.Push(2);
+            ints.Push(3);
+            ints.Push(4);
+            ints.Push(0);
+            ints.Push(-1);
+            ints.Push(80);
+
+            ints.Pop();
+            ints.Pop();
+
+            // Creating a test list
+            List<int> list = new List<int> { 1, 2, 3, 4, 0 };
+
+            Assert.Equal(ints.StackValues, list);
+        }
+
+        [Fact]
+        public void TestStackPeek()
+        {
+            StackArray<int> ints = new StackArray<int>();
+
+            ints.Push(1);
+            ints.Push(2);
+            ints.Push(3);
+            ints.Push(4);
+            ints.Push(0);
+            ints.Push(-1);
+            ints.Push(80);
+
+            Assert.Equal(80, ints.Peek());
+        }
+    }
+}
---
+++ Algorithms/Section9/StackArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Section9
+{
+    public class StackArray<T>
+    {
+        public List<T> StackValues = new List<T>();
+        public StackArray() { }
+
+        public T Peek()
+        {
+            return StackValues.Last();
+        }
+
+        public T Push(T value)
+        {
+            StackValues.Add(value);
+            return value;
+        }
+
+        public T Pop()
+        {
+            T result = StackValues.Last();
+            StackValues.RemoveAt(StackValues.Count - 1);
+            return result;
+        }
+    }
+}
