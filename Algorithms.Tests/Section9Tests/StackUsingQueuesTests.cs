f82992799cac00b0687b336f5a498136b847f324
Ulgamon <ilicjustin@gmail.com>
2025-11-24 18:38:46 +0100

    Stack using queues.

---
+++ Algorithms.Tests/Section9Tests/StackUsingQueuesTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Section9;
+
+namespace Algorithms.Tests.Section9Tests
+{
+    public class StackUsingQueuesTests
+    {
+        public class StackTests
+        {
+            [Fact]
+            public void TestStack()
+            {
+                StackUsingQueues<int> ints = new StackUsingQueues<int>();
+
+                ints.Push(1);
+                ints.Push(2);
+                ints.Push(3);
+                ints.Push(4);
+                ints.Push(0);
+                ints.Push(-1);
+                ints.Push(80);
+
+                // Creating a test list
+                List<int> list = new List<int> { 1, 2, 3, 4, 0, -1, 80 };
+
+                Assert.Equal(ints.QueueList(), list);
+            }
+
+            [Fact]
+            public void TestStackPop()
+            {
+                StackUsingQueues<int> ints = new StackUsingQueues<int>();
+
+                ints.Push(1);
+                ints.Push(2);
+                ints.Push(3);
+                ints.Push(4);
+                ints.Push(0);
+                ints.Push(-1);
+                ints.Push(80);
+
+                ints.Pop();
+                ints.Pop();
+
+                // Creating a test list
+                List<int> list = new List<int> { 1, 2, 3, 4, 0 };
+
+                Assert.Equal(ints.QueueList(), list);
+            }
+
+            [Fact]
+            public void TestStackTop()
+            {
+                StackUsingQueues<int> ints = new StackUsingQueues<int>();
+
+                ints.Push(1);
+                ints.Push(2);
+                ints.Push(3);
+                ints.Push(4);
+                ints.Push(0);
+                ints.Push(-1);
+                ints.Push(80);
+
+                Assert.Equal(80, ints.Top());
+            }
+        }
+    }
+}
---
+++ Algorithms/Section9/StackUsingQueues.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Section9
+{
+    public class StackUsingQueues<T>
+    {
+        public Queue<T> Queue1 = new Queue<T>();
+        public Queue<T> Queue2 = new Queue<T>();
+
+        public StackUsingQueues() { }
+
+        public void Push(T x)
+        {
+            Queue1.Enqueue(x);
+        }
+
+        public T Pop()
+        {
+            if (Queue1.Count > 0 && Queue2.Count == 0)
+            {
+                while (Queue1.Count > 1)
+                {
+                    Queue2.Enqueue(Queue1.Dequeue());
+                }
+                T result = Queue1.Dequeue();
+                while (Queue2.Count > 0)
+                {
+                    Queue1.Enqueue(Queue2.Dequeue());
+                }
+                return result;
+            }
+            return default;
+        }
+
+        public T Top()
+        {
+            if (Queue1.Count > 0 && Queue2.Count == 0)
+            {
+                while (Queue1.Count > 1)
+                {
+                    Queue2.Enqueue(Queue1.Dequeue());
+                }
+                T result = Queue1.Dequeue();
+                Queue2.Enqueue(result);
+                while (Queue2.Count > 0)
+                {
+                    Queue1.Enqueue(Queue2.Dequeue());
+                }
+                return result;
+            }
+            return default;
+        }
+
+        public bool Empty()
+        {
+            return Queue1.Count > 0 || Queue2.Count > 0 ? false : true;
+        }
+
+        public List<T> QueueList()
+        {
+
+            List<T> list1 = Queue1.ToList();
+
+            return list1;
+        }
+    }
+}
+
