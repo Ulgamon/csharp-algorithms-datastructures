1057e3bc9fc5ecc2061c8551bb1e6228cebb00ef
Ulgamon <ilicjustin@gmail.com>
2026-01-15 20:08:55 +0100

    Fibonacci iterative and recursive.

---
+++ Algorithms.Tests/Section12/FibonacciTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Section12;
+
+namespace Algorithms.Tests.Section12
+{
+    public class FibonacciTests
+    {
+        [Fact]
+        public void FibonacciTestValid()
+        {
+            int ficonacci = Fibonacci.FibonacciFunction(10);
+
+            int result = 55;
+
+            Assert.Equal(result, ficonacci);
+        }
+
+        [Fact]
+        public void FibonacciTestValidMiddle()
+        {
+            int ficonacci = Fibonacci.FibonacciFunction(16);
+
+            int result = 987;
+
+            Assert.Equal(result, ficonacci);
+        }
+
+        [Fact]
+        public void FibonacciTestRecursiveValid()
+        {
+            int ficonacci = Fibonacci.FibonacciRecursive(10);
+
+            int result = 55;
+
+            Assert.Equal(result, ficonacci);
+        }
+
+        [Fact]
+        public void FibonacciTestRecursiveValidMiddle()
+        {
+            int ficonacci = Fibonacci.FibonacciRecursive(16);
+
+            int result = 987;
+
+            Assert.Equal(result, ficonacci);
+        }
+    }
+}
---
+++ Algorithms/Section12/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Section12
+{
+    public static class Fibonacci
+    {
+        public static int FibonacciFunction(int n)
+        {
+            int current = 1;
+            int prev = 0;
+            int temp = 0;
+            for (int i = 1; i < n; i++)
+            {
+                temp = current;
+                current += prev;
+                prev = temp;
+            }
+
+            return current;
+        }
+
+        public static int FibonacciRecursive(int n)
+        {
+            if (n < 2) return n ;
+
+
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+    }
+}
